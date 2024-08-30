a41f211dff5bfb9149cb40aa32642ccf7463b9b2
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-08-30 16:34:59 -0600

    manejo del pipe date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Configuración del locale app
+//! formas de importar el locale de Méxcio (es-MX) en la aplicación de
+import localeEsMX from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsMX);
 
 @NgModule({
   declarations: [
@@ -17,7 +24,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    //! forma de como establecer como idioma predeterminado del español de México en toda la aplicación
+    {  provide: LOCALE_ID, useValue: 'es-MX' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
